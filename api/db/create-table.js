3a38f08397769e11f415294b56c76f0c342e5f45
Elijah Bobzom <ebobzom@gmail.com>
2019-07-12 14:52:32 +0100

    chore:add signin route [delivers #167018291]

--- api/db/create-table.js
+++ api/db/create-table.js
@@ -8,12 +8,7 @@ const con = new pg.Client({
   port: process.env.DB_PORT,
 });
 con.connect();
-con.query('drop table if exists person', (err, res) => {
-  if (err) {
-    console.log(e);
-  }
-  console.log(res);
-});
+con.query('drop table if exists person');
 
 con.query('create table person(user_id serial primary key not null, first_name varchar(50) not null, last_name varchar(50) not null, password varchar not null, is_admin boolean not null, email varchar(50) not null);', () => {
   con.end();
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const { createUser } = require('./api/db/dbGethersAndSetters');
+const { createUser, signinUser } = require('./api/db/dbGethersAndSetters');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.post('/api/v1/auth/signup', createUser);
+app.post('/api/v1/auth/signin', signinUser);
 
 app.listen(port, () => `Server runnig on port ${port}`);
 module.exports = app;
