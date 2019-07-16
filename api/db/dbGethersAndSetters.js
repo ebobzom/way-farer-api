8a70afb8f54bc426cc7e3544bc9a17ef84a442ef
Elijah Bobzom <ebobzom@gmail.com>
2019-07-17 00:29:08 +0100

    bug:add connection logic [delivers 167019709]

--- api/db/dbGethersAndSetters.js
+++ api/db/dbGethersAndSetters.js
@@ -6,7 +6,7 @@ const { config } = require('dotenv');
 config();
 
 const pool = new Pool({
-  connectionString = 'postgres://bqjrahuhoquxfh:147a9e97ab16fbd118b45157abe687675f90601938dd366f6fcd5aaa4e2a6556@ec2-174-129-227-51.compute-1.amazonaws.com:5432/dbuva3dq3grjk5'
+  connectionString: 'postgres://bqjrahuhoquxfh:147a9e97ab16fbd118b45157abe687675f90601938dd366f6fcd5aaa4e2a6556@ec2-174-129-227-51.compute-1.amazonaws.com:5432/dbuva3dq3grjk5',
   // user: process.env.DB_USER,
   // host: process.env.HOST,
   // database: process.env.DB,
